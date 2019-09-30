60ae3e63599364e9cbed714c27624f5650dbf993
Glitch (hyper-nannyberry) <none>
2019-09-30 17:21:43 +0000

    🎃📯 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+app.set('view engine','ejs')
+app.use(express.static('public'));
+app.set('views', '/app/public/')
+app.get('/', function(req, res) {
+  res.render("index")
+});
+// listen for requests :)
+const listener = app.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
